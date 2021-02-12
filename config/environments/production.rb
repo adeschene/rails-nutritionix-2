005b5efadb43713dbad2be6410697fcd2c7823c2
adeschene <alecdeschene@gmail.com>
2021-02-11 16:16:51 -0800

    fix production action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,11 +119,10 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.default_url_options = { host: 'rails-nutritionix-2.herokuapp.com/' }
-  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
-    :domain => 'herokuapp.com',
+    :domain => 'rails-nutritionix-2.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
