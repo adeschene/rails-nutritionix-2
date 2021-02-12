0c89379d24640726ceb5eaa4b3d9f055f0d4d87a
adeschene <alecdeschene@gmail.com>
2021-02-12 00:29:10 -0800

    fix production action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,7 +122,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
-    :domain => 'rails-nutritionix-2.herokuapp.com',
+    :domain => 'herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
